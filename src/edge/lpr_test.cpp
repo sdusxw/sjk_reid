32064075b499f093545464c4e2b076021c0a206a
sdusxw <sxw@sdboon.com>
2019-10-02 16:17:27 +0800

    yuv3

--- src/edge/lpr_test.cpp
+++ src/edge/lpr_test.cpp
@@ -8,6 +8,35 @@
 
 using namespace std;
 
+#include <sys/time.h>
+
+#include <time.h>
+#include <unistd.h>
+
+double t1 =0;
+double t2 = 0;
+double  getCurrentTime()
+{
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+    return (double)tv.tv_sec * 1000.0 + tv.tv_usec / 1000.0;
+}
+
+void starttime()
+{
+    t1 = getCurrentTime();
+}
+
+
+void endtime(char *title)
+{
+    t2  = getCurrentTime ();
+    double timeuse = t2 -t1 ;
+    
+    printf ("title  %s usetime =%2.2f ms\n" ,title,timeuse);
+    
+}
+
 int main(int argc, char ** argv)
 {
     if(argc < 2)
@@ -33,19 +62,25 @@ int main(int argc, char ** argv)
     EyeJpegCodec ejc;
     int c=4;
     int w=0, h=0;
+    starttime();
     bool ret = ejc.JpegUnCompress((const char *)buffer.c_str(), size, (char *)argb_data,
                                   4096*2160*3, w, h, c);
+    endtime("jpeg decode");
     if(ret)
     {
         cout << "w:\t" << w << "\th:\t" << h << endl;
         uint8_t *y_data=(uint8_t *)malloc(w*h);
         uint8_t *uv_data=(uint8_t *)malloc(w*h/2);
+        starttime();
         libyuv::ARGBToNV12((const uint8_t*)argb_data, w*c, y_data, w, uv_data, w, w, h);
+        endtime("jpeg -> nv12");
         FILE * outfile;
+        starttime();
         outfile = fopen("p.yuv", "wb" );
         fwrite(y_data, w*h, 1, outfile);
         fwrite(uv_data, w*h/2, 1, outfile);
         fclose(outfile);
+        endtime("save yuv");
     }
     else
         cout << "jpeg decode error!" << endl;
